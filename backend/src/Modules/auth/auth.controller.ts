5f155e48cdc8a4ba7e1dc370fe991e1916239ff8
FaroukMBen <FaroukMohamed.Bendeddouche@outlook.com>
2025-05-26 18:18:35 +0200

    fix(auth) : Commenting the auth in backend because it is now fully handeled by the supabase api

--- backend/src/Modules/auth/auth.controller.ts
+++ backend/src/Modules/auth/auth.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller } from '@nestjs/common';
+
+/**
+ * The authentification provided by the supabase api is used to handle the login and signup
+ * so no need to redefine the functions in the authController wich means that all the auth module is handled.
+ */
 
 @Controller('profile')
 export class ProfileController {
+  /*
   @UseGuards(AuthGuard('jwt'))
   @Get()
   getProfile(@Request() req) {
     return req.user;
   }
+  */
 }
